7d719ccc9a39f1884a428255055fa7ba9087fe44
DJ Daugherty <ddaugher@gmail.com>
2013-04-08 20:38:57 -0400

    init of item should set original price to passed in value

--- src/test/scala/redPencilScalaSpec.scala
+++ src/test/scala/redPencilScalaSpec.scala
@@ -2,13 +2,13 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 
 class redPencilScalaSpec extends FunSuite with BeforeAndAfter {
 
-  //var i: redPencilScala = _
+  var i: redPencilScala = _
 
   before {
-    //i = new redPencilScala
+    i = new redPencilScala(100.0)
   }
 
-  test("first test") {
-    assert(true === false)
+  test("init of item should set original price to passed in value") {
+    assert(100.0 === i.originalPrice)
   }
 }
