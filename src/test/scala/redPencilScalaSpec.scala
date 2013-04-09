00c91f10b741cafe6c01b8e916a67a5a42cad89e
DJ Daugherty <ddaugher@gmail.com>
2013-04-08 20:30:53 -0400

    initial add to the repository... using sbt as well

---
+++ src/test/scala/redPencilScalaSpec.scala
@@ -0,0 +1,14 @@
+import org.scalatest.{BeforeAndAfter, FunSuite}
+
+class redPencilScalaSpec extends FunSuite with BeforeAndAfter {
+
+//var i: redPencilScala = _
+
+before {
+  //i = new redPencilScala
+}
+
+test("first test") {
+  assert(true === false)
+}
+}
