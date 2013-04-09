87700f63bc041dd29cc7207a71a37c61c1c7f0a8
DJ Daugherty <ddaugher@gmail.com>
2013-04-08 20:43:30 -0400

    passed

--- src/main/scala/redPencilScala.scala
+++ src/main/scala/redPencilScala.scala
@@ -2,8 +2,10 @@ class redPencilScala(private val _originalPrice:Double = 0.0) {
 
   private var _originalPriceDuration: Int = 0
   private var _salePrice: Double = 0.0
+  private var _saleDuration: Int = 0
 
   def originalPrice:Double = _originalPrice
   def originalPriceDuration:Int = _originalPriceDuration
   def salePrice:Double = _salePrice
+  def saleDuration:Int = _saleDuration
 }
